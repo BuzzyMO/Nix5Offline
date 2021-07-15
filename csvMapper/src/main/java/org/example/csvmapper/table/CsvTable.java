d9490efb216465fd8aa44652273d9413235d5d89
BuzzyMO <oleksandr.lynnyk@nure.ua>
2021-07-16 02:00:47 +0300

    data structure that represent csv table

---
+++ csvMapper/src/main/java/org/example/csvmapper/table/CsvTable.java
@@ -0,0 +1,40 @@
+package org.example.csvmapper.table;
+
+import org.example.csvmapper.parser.CsvParser;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CsvTable implements Table{
+    private final String[] header;
+    private final List<String[]> body;
+
+    public CsvTable(CsvParser parser){
+        header = parser.getHeader();
+        body = parser.getBody();
+    }
+
+    public String get(int row, String colName){
+        int col = indexOfCol(colName);
+        return get(row, col);
+    }
+
+    public String get(int row, int col){
+        String[] fullRow = body.get(row);
+        return fullRow[col];
+    }
+
+    private int indexOfCol(String colName){
+        for (int i = 0; i < header.length; i++) {
+            if(header[i].equals(colName))
+                return i;
+        }
+        throw new IllegalArgumentException("Column doesn't exist: " + colName);
+    }
+
+    public List<String> getHeader(){
+        return Arrays.stream(header)
+                .collect(Collectors.toList());
+    }
+}
---
+++ csvMapper/src/main/java/org/example/csvmapper/table/Table.java
@@ -0,0 +1,9 @@
+package org.example.csvmapper.table;
+
+import java.util.List;
+
+public interface Table {
+    String get(int row, String colName);
+    String get(int row, int col);
+    List<String> getHeader();
+}

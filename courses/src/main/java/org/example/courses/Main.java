537a3d7f754f165da5fc5c15bb82dc60f3412b3e
BuzzyMO <oleksandr.lynnyk@nure.ua>
2021-07-31 13:18:45 +0300

    entity manager as session, studentId from args

--- courses/src/main/java/org/example/courses/Main.java
+++ courses/src/main/java/org/example/courses/Main.java
@@ -1,18 +1,22 @@
 package org.example.courses;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import javax.persistence.EntityManager;
-
 public class Main {
     public static void main(String[] args) {
+        long studentId;
+        if (args.length > 0) {
+            studentId = Long.parseLong(args[0]);
+        } else {
+            throw new RuntimeException("You should set studentId as arg");
+        }
         Configuration conf = new Configuration().configure();
-        try(SessionFactory sessionFactory = conf.buildSessionFactory()){
-            EntityManager entityManager = sessionFactory.createEntityManager();
+        try (SessionFactory sessionFactory = conf.buildSessionFactory()) {
+            Session entityManager = (Session) sessionFactory.createEntityManager();
             CourseEntityManager courseEntityManager = new CourseEntityManager();
-            courseEntityManager.findAndPrintUpcomingLesson(entityManager, 1L);
-            entityManager.close();
+            courseEntityManager.findAndPrintUpcomingLesson(entityManager, studentId);
         }
     }
 }
